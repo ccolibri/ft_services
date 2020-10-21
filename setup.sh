6580dbfd273d7c9bb3b05de4d0ea0bae466e2181
Admin <admin@Mac-Admin.local>
2020-10-21 15:08:26 +0300

    service

--- setup.sh
+++ setup.sh
@@ -2,53 +2,30 @@ minikube start --driver=hyperkit --cpus=2 --memory='3072'
 minikube addons enable metrics-server
 minikube addons enable dashboard
 eval $(minikube -p minikube docker-env)
+
 minikube addons enable metallb
 
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 
 kubectl apply -f srcs/metallb/metallb.yaml
 
-cd srcs/nginx/
-docker build -t nginx-image .
-kubectl apply -f nginx.yaml
-cd ../..
+docker build -t nginx-image ./srcs/nginx
+docker build -t ftps-image ./srcs/ftps
+docker build -t grafana-image ./srcs/grafana
+docker build -t influxdb-image ./srcs/influxdb
+docker build -t mysql-image ./srcs/mysql
+docker build -t phpmyadmin-image ./srcs/phpmyadmin
+docker build -t telegraf-image ./srcs/telegraf
+docker build -t wordpress-image ./srcs/wordpress
 
-cd srcs/ftps/
-docker build -t ftps-image .
-kubectl apply -f ftps.yaml
-cd ../..
-
-cd srcs/mysql
-docker build -t mysql-image .
-kubectl apply -f mysql.yaml
-cd ../..
-
-cd srcs/phpmyadmin
-docker build -t phpmyadmin-image .
-kubectl apply -f phpmyadmin.yaml
-cd ../..
-
-cd srcs/wordpress
-docker build -t wordpress-image .
-kubectl apply -f wordpress.yaml
-cd ../..
-
-cd srcs/influxdb
-docker build -t influxdb-image .
-kubectl apply -f influxdb_config.yaml
-kubectl apply -f influxdb.yaml
-cd ../..
-
-cd srcs/telegraf
-docker build -t telegraf-image .
-kubectl apply -f telegraf_config.yaml
-kubectl apply -f telegraf.yaml
-cd ../..
-
-cd srcs/grafana
-docker build -t grafana-image .
-kubectl apply -f grafana_config.yaml
-kubectl apply -f grafana.yaml
-cd ../..
+kubectl apply -f ./srcs/nginx/nginx.yaml
+kubectl apply -f ./srcs/ftps/ftps.yaml
+kubectl apply -f ./srcs/grafana/grafana_config.yaml
+kubectl apply -f ./srcs/grafana/grafana.yaml
+kubectl apply -f ./srcs/influxdb/influxdb_config.yaml
+kubectl apply -f ./srcs/influxdb/influxdb.yaml
+kubectl apply -f ./srcs/mysql/mysql.yaml
+kubectl apply -f ./srcs/phpmyadmin/phpmyadmin.yaml
+kubectl apply -f ./srcs/wordpress/wordpress.yaml
+kubectl apply -f ./srcs/telegraf/telegraf_config.yaml
+kubectl apply -f ./srcs/telegraf/telegraf.yaml
