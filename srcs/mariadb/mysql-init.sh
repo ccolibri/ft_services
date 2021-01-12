123d89cefd2d223f9ea14ce0ecf44558521f79cc
Admin <admin@Mac-Admin.local>
2021-01-12 16:42:15 +0300

    wp

--- srcs/mariadb/mysql-init.sh
+++ srcs/mariadb/mysql-init.sh
@@ -3,7 +3,7 @@ do
 	sleep 1
 done
 echo "CREATE DATABASE wordpress;" | mysql -u root --skip-password
-echo "CREATE USER 'admin'@'%' IDENTIFIED BY 'admin';" | mysql -u root --skip-password
-echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'admin'@'%' WITH GRANT OPTION;" | mysql -u root --skip-password
+echo "CREATE USER 'ccaptain'@'%' IDENTIFIED BY 'ccaptain';" | mysql -u root --skip-password
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'ccaptain'@'%' WITH GRANT OPTION;" | mysql -u root --skip-password
 echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
 mysql -u root wordpress < wordpress-dump.sql
